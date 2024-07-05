6aef8ac0884f4303066d2025b808ccb3e26efb1f
jihyeon <jhbjhjh22@gmail.com>
2024-07-05 14:46:29 +0900

    Add 환자검색api

--- patients/urls.py
+++ patients/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from .views import *
 
 urlpatterns = [
     path('add/', views.add_patient, name='add_patient'),
     path('list/', views.patients_list, name='patients_list'),
+    path('list2/', PatientListAPIView.as_view(), name='patient-list2'),
+    path('list2/search/', PatientSearchAPIView.as_view(), name='patient-search'),
 ]
--- patients/views.py
+++ patients/views.py
@@ -1,8 +1,10 @@
 from datetime import date
-from django.shortcuts import render
- 
 from django.shortcuts import render, redirect
 from .models import Patient
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import PatientSerializer
  
 def add_patient(request):
     if request.method == 'POST':
@@ -23,3 +25,27 @@ def add_patient(request):
 def patients_list(request):
     patients = Patient.objects.all()
     return render(request, 'patients/patients_list.html', {'patients': patients}) #환자 조회 리스트
+
+class PatientSearchAPIView(APIView):
+    def get(self, request, format=None):
+        # URL 쿼리 파라미터에서 이름과 생년월일(YYMMDD)을 가져옴
+        name = request.query_params.get('name', None)
+        bday = request.query_params.get('patient_bday', None)
+
+        # 이름과 생일이 모두 제공되어야 함
+        if not name or not bday:
+            return Response({'error': '이름과 생년월일을 모두 입력하세요.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # 이름과 생일로 환자를 찾음 (이름은 대소문자 구분 없이 검색)
+        patients = Patient.objects.filter(patient_name__iexact=name, patient_bday=bday)
+        
+        if patients.exists():
+            serializer = PatientSerializer(patients, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({'message': '일치하는 환자가 없습니다.'}, status=status.HTTP_404_NOT_FOUND)
+class PatientListAPIView(APIView):
+    def get(self, request, format=None):
+        patients = Patient.objects.all()
+        serializer = PatientSerializer(patients, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
