4e4af497ed94989add8276bab37f17e1fb989ca4
jihyeon <jhbjhjh22@gmail.com>
2024-07-05 14:55:36 +0900

    add 환자조회

--- patients/urls.py
+++ patients/urls.py
@@ -5,6 +5,6 @@ from .views import *
 urlpatterns = [
     path('add/', views.add_patient, name='add_patient'),
     path('list/', views.patients_list, name='patients_list'),
-    path('list2/', PatientListAPIView.as_view(), name='patient-list2'),
-    path('list2/search/', PatientSearchAPIView.as_view(), name='patient-search'),
+    path('list2/', PatientListAPIView.as_view(), name='patient-list2'), # 전체환자 조회
+    path('list2/search/', PatientSearchAPIView.as_view(), name='patient-search'), #이름과 생년월일로 조회 GET /patients/list2/search/?name=김환자&patient_bday=800101
 ]
