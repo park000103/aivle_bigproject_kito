5d52fbfdfb893e9a6efc797e5e80a344ce328397
park000103 <park000103@gmail.com>
2024-07-08 14:16:58 +0900

    reservations app에 ai모델 추가

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
+from .views import get_recommendation
 
 urlpatterns = [
-
+    path('recommend/', get_recommendation, name='get_recommendation'),
 ]
