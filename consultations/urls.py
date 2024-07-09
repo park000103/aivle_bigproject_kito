38d2fd217db83104e5ccfaf5d632c40b3e1b81ed
jihyeon <jhbjhjh22@gmail.com>
2024-07-09 10:10:16 +0900

    add 수납 전체리스트

--- consultations/urls.py
+++ consultations/urls.py
@@ -3,6 +3,7 @@ from . import views
 from .views import ConsultationPaymentListView
 
 urlpatterns = [
+    path('bfpayment/', ConsultationPaymentListView.as_view(), name='consultation-payment-list'),
     path('bfpayment/<int:patient_id>/', ConsultationPaymentListView.as_view(), name='consultation-payment-list'),#수납전 당일 진료 리스트
 
 ]
