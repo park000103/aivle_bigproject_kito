0b3d5a77ba0147f568163fae75518a7938cfdb1c
jihyeon <jhbjhjh22@gmail.com>
2024-07-11 17:22:58 +0900

    환자, 챗봇 수정

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,20 +1,24 @@
+import base64
 from django.shortcuts import render
 from django.http import JsonResponse
 import requests
 from django.views.decorators.csrf import csrf_exempt
-import json
+from google.cloud import speech
 
-VOICEFLOW_API = ''  # voiceflow api키 넣기
+VOICEFLOW_API = ''  # Voiceflow API 키 넣기
 
 def index(request):
     user_id = 'unique_user_id'  # 사용자 고유 ID 설정
     initial_message = get_initial_message(user_id)
     return render(request, 'chatbot/index.html', {'initial_message': initial_message})
 
-def get_initial_message(user_id):
-    # Voiceflow API에 요청하여 초기 메시지를 가져옴
-    url = f"https://general-runtime.voiceflow.com/state/user/{user_id}/interact?logs=off"
+def speech_page(request):
+    user_id = 'unique_user_id'  # 사용자 고유 ID 설정
+    initial_message = get_initial_message(user_id)
+    return render(request, 'chatbot/speech.html', {'initial_message': initial_message})
 
+def get_initial_message(user_id):
+    url = f"https://general-runtime.voiceflow.com/state/user/{user_id}/interact?logs=off"
     payload = {
         "action": {"type": "launch"},
         "config": {
@@ -28,29 +32,24 @@ def get_initial_message(user_id):
     headers = {
         "accept": "application/json",
         "content-type": "application/json",
-        "Authorization": VOICEFLOW_API  # Voiceflow API 키로 교체하세요
+        "Authorization": VOICEFLOW_API
     }
-
     response = requests.post(url, json=payload, headers=headers)
     print(response.text)
-
     if response.status_code == 200:
         data = response.json()
-        # 응답 데이터 구조를 확인 후 message 부분 추출
         for item in data:
             if 'message' in item['payload']:
                 return item['payload']['message']
-    
-    return None  # 실패 시 None 반환 또는 예외 처리
+    return None
 
 @csrf_exempt
 def send_message(request):
     if request.method == "POST":
         message = request.POST.get('message')
-        user_id = 'unique_user_id'  # 사용자 고유 ID 설정
+        user_id = 'unique_user_id'
         print(message)
         url = f"https://general-runtime.voiceflow.com/state/user/{user_id}/interact?logs=off"
-
         payload = {
             "action": {
                 "type": "text",
@@ -67,26 +66,23 @@ def send_message(request):
         headers = {
             "accept": "application/json",
             "content-type": "application/json",
-            "Authorization": VOICEFLOW_API  # Voiceflow API 키로 교체하세요
+            "Authorization": VOICEFLOW_API
         }
-
         response = requests.post(url, json=payload, headers=headers)
         print(response.text)
-
         if response.status_code == 200:
             try:
                 data = response.json()
                 messages = []
-                # Voiceflow API에서 응답으로 받은 데이터 처리
                 if isinstance(data, list):
                     for item in data:
                         if 'message' in item.get('payload', {}):
                             message = item['payload']['message']
-                            delay = item.get('payload', {}).get('delay', 0)  # delay 정보 추출
+                            delay = item.get('payload', {}).get('delay', 0)
                             messages.append({'message': message, 'delay': delay})
                 elif isinstance(data, dict) and 'message' in data.get('payload', {}):
                     message = data['payload']['message']
-                    delay = data.get('payload', {}).get('delay', 0)  # delay 정보 추출
+                    delay = data.get('payload', {}).get('delay', 0)
                     messages.append({'message': message, 'delay': delay})
                 return JsonResponse({'messages': messages})
             except ValueError as e:
@@ -94,4 +90,32 @@ def send_message(request):
         else:
             return JsonResponse({'error': 'Failed to interact with Voiceflow API'}, status=response.status_code)
     else:
-        return JsonResponse({'error': 'Invalid request method'}, status=405)  # POST 요청이 아닐 경우 에러 처리
+        return JsonResponse({'error': 'Invalid request method'}, status=405)
+
+@csrf_exempt
+def transcribe(request):
+    if request.method == 'POST':
+        audio_data = request.POST.get('audio_data')
+        if not audio_data:
+            return JsonResponse({'error': 'audio_data is missing'}, status=400)
+
+        audio_content = base64.b64decode(audio_data.split(',')[1])
+        client = speech.SpeechClient()
+        audio = speech.RecognitionAudio(content=audio_content)
+        config = speech.RecognitionConfig(
+            encoding=speech.RecognitionConfig.AudioEncoding.WEBM_OPUS,
+            sample_rate_hertz=48000,
+            language_code="ko-KR"
+        )
+        try:
+            response = client.recognize(config=config, audio=audio)
+            transcript = ""
+            for result in response.results:
+                transcript += result.alternatives[0].transcript
+            return JsonResponse({'transcript': transcript})
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=400)
+    else:
+        user_id = 'unique_user_id'
+        initial_message = get_initial_message(user_id)
+        return render(request, 'chatbot/speech.html', {'initial_message': initial_message})
